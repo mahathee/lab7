80d803acf80f031881fdbaf03b082db5f9b6e7fe
Mahathee Chetlapalli <mchetlap@ucsd.edu>
2014-02-20 17:55:43 +0000

    mongoDB lab. it works! woohoo! :)

--- routes/project.js
+++ routes/project.js
@@ -3,8 +3,10 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
-  // query for the specific project and
-  // call the following callback
+    
+  models.Project
+    .find({"_id": projectID}) // query for the specific project and
+    .exec(afterQuery);   // call the following callback
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -16,13 +18,31 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
-  // make a new Project and save it to the DB
+  var newProject = new models.Project({
+    "title": form_data['project_title'],
+    "date": form_data['date'],
+    "summary": form_data['summary'],
+    "image": form_data['image_url']}); // make a new Project 
+  
+  newProject.save(afterSaving); //and save it to the DB
+
   // YOU MUST send an OK response w/ res.send();
+  function afterSaving(err) {
+    if(err) { console.log(err); res.send(500);}
+    res.send(200);
+  }
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+    models.Project
+      .find({"_id": projectID})// find the project and 
+      .remove() //remove it
+      .exec(afterRemoving);
 
-  // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  function afterRemoving(err){
+    if(err){console.log(err); res.send(500);}
+    res.send(200);
+  }
 }
